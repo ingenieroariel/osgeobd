13258d2fbeac5397cee95f946c684425ea7b16a0
Ariel Nuñez <ingenieroariel@gmail.com>
2015-10-17 15:30:48 +0600

    Added restaurantfinder application

--- Makefile
+++ Makefile
@@ -1,13 +1,21 @@
 OSMOSIS='osmosis/bin/osmosis'
+DB='osmbangladesh'
 
 # Download osm information from the whole country
 # updated in the last 24 hours.
 bangladesh.pbf:
 	wget -O $@ "http://download.geofabrik.de/asia/bangladesh-latest.osm.pbf"
 
+buildings.pbf: bangladesh.pbf
+	$(OSMOSIS) --read-pbf-fast file="$<" --tf accept-ways "building=*"  --write-pbf file="$@"
 
 restaurants.pbf: bangladesh.pbf
 	$(OSMOSIS) --read-pbf-fast file="$<" --nkv keyValueList="amenity.restaurant,amenity.restaurants" --tf reject-ways --tf reject-relations --write-pbf file="$@"
 
-restaurants.sql: restaurants.pbf
+%.sql: %.pbf
 	ogr2ogr -f PGDump $@ $< -lco COLUMN_TYPES=other_tags=hstore --config       OSM_CONFIG_FILE conf/$(basename $@).ini
+
+%.postgis: %.sql
+	psql -f $< $(DB)
+	psql -f conf/$(basename $@)_alter.sql $(DB)
+	psql -f conf/clean.sql -q $(DB)
---
+++ restaurantfinder/osm/models.py
@@ -0,0 +1,58 @@
+from __future__ import unicode_literals
+
+from django.contrib.gis.db import models
+
+
+class Building(models.Model):
+    id = models.IntegerField(primary_key=True, db_column='ogc_fid')
+    geom = models.MultiPolygonField(db_column='wkb_geometry', blank=True, null=True)
+    osm_id = models.CharField(max_length=255, blank=True, null=True)
+    osm_way_id = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    type = models.CharField(max_length=255, blank=True, null=True)
+    aeroway = models.CharField(max_length=255, blank=True, null=True)
+    amenity = models.CharField(max_length=255, blank=True, null=True)
+    admin_level = models.CharField(max_length=255, blank=True, null=True)
+    barrier = models.CharField(max_length=255, blank=True, null=True)
+    boundary = models.CharField(max_length=255, blank=True, null=True)
+    building = models.CharField(max_length=255, blank=True, null=True)
+    craft = models.CharField(max_length=255, blank=True, null=True)
+    geological = models.CharField(max_length=255, blank=True, null=True)
+    historic = models.CharField(max_length=255, blank=True, null=True)
+    land_area = models.CharField(max_length=255, blank=True, null=True)
+    landuse = models.CharField(max_length=255, blank=True, null=True)
+    leisure = models.CharField(max_length=255, blank=True, null=True)
+    man_made = models.CharField(max_length=255, blank=True, null=True)
+    military = models.CharField(max_length=255, blank=True, null=True)
+    natural = models.CharField(max_length=255, blank=True, null=True)
+    office = models.CharField(max_length=255, blank=True, null=True)
+    place = models.CharField(max_length=255, blank=True, null=True)
+    shop = models.CharField(max_length=255, blank=True, null=True)
+    sport = models.CharField(max_length=255, blank=True, null=True)
+    tourism = models.CharField(max_length=255, blank=True, null=True)
+    other_tags = models.TextField(blank=True, null=True)  # This field type is a guess.
+    objects = models.GeoManager()
+
+    class Meta:
+        managed = False
+        db_table = 'buildings'
+
+
+class Restaurant(models.Model):
+    ogc_fid = models.IntegerField(blank=True, null=True)
+    wkb_geometry = models.PointField(blank=True, null=True)
+    osm_id = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    barrier = models.CharField(max_length=255, blank=True, null=True)
+    highway = models.CharField(max_length=255, blank=True, null=True)
+    ref = models.CharField(max_length=255, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    is_in = models.CharField(max_length=255, blank=True, null=True)
+    place = models.CharField(max_length=255, blank=True, null=True)
+    man_made = models.CharField(max_length=255, blank=True, null=True)
+    other_tags = models.TextField(blank=True, null=True)  # This field type is a guess.
+    objects = models.GeoManager()
+
+    class Meta:
+        managed = False
+        db_table = 'restaurants'
